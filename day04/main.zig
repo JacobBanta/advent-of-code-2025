d8cad53f4bea268bf428ec27d4cc654377528e9b
Jacob Banta <jacob@banta.email>
2025-12-05 14:56:45 -0500

    day 4 significantly reduced to amount of branches

--- day04/main.zig
+++ day04/main.zig
@@ -1,10 +1,15 @@
 const std = @import("std");
 pub fn main() !void {
-    const input = @embedFile("input.txt");
-    const lineLen = std.mem.indexOfScalar(u8, input, '\n').? + 1;
-    const lines = @divExact(input.len, lineLen);
+    const rawinput = @embedFile("input.txt");
+    const lineLen = std.mem.indexOfScalar(u8, rawinput, '\n').? + 1;
+    var padded = try std.ArrayList(u8).initCapacity(std.heap.page_allocator, rawinput.len + lineLen * 2);
+    padded.appendNTimesAssumeCapacity('\n', lineLen);
+    padded.appendSliceAssumeCapacity(rawinput);
+    padded.appendNTimesAssumeCapacity('\n', lineLen);
+    const input = padded.items;
 
-    var neighbors = [_]i8{0} ** input.len;
+    var neighbors = try std.heap.page_allocator.alloc(i8, rawinput.len + lineLen * 2);
+    @memset(neighbors, 0);
 
     var count: usize = 0;
     for (input, 0..) |char, index| {
@@ -13,49 +18,17 @@ pub fn main() !void {
                 neighbors[index] = 64;
             },
             '@' => {
-                const x = @mod(index, lineLen);
-                const y = @divFloor(index, lineLen);
-                // This is a lot, but neighbor checking looks rough
-                if (x >= 1) {
-                    if (y >= 1) {
-                        if (input[index - lineLen - 1] == '@') {
-                            neighbors[index] += 1;
-                        }
-                    }
-                    if (input[index - 1] == '@') {
-                        neighbors[index] += 1;
-                    }
-                    if (y < lines - 1) {
-                        if (input[index + lineLen - 1] == '@') {
-                            neighbors[index] += 1;
-                        }
-                    }
-                }
-                if (y >= 1) {
-                    if (input[index - lineLen] == '@') {
-                        neighbors[index] += 1;
-                    }
-                }
-                if (y < lines - 1) {
-                    if (input[index + lineLen] == '@') {
-                        neighbors[index] += 1;
-                    }
-                }
-                if (x < lineLen - 1) {
-                    if (y >= 1) {
-                        if (input[index - lineLen + 1] == '@') {
-                            neighbors[index] += 1;
-                        }
-                    }
-                    if (input[index + 1] == '@') {
-                        neighbors[index] += 1;
-                    }
-                    if (y < lines - 1) {
-                        if (input[index + lineLen + 1] == '@') {
-                            neighbors[index] += 1;
-                        }
-                    }
-                }
+                const nearNeighbors = @as(@Vector(8, u8), [8]u8{
+                    input[index - lineLen - 1],
+                    input[index - 1],
+                    input[index + lineLen - 1],
+                    input[index - lineLen],
+                    input[index + lineLen],
+                    input[index - lineLen + 1],
+                    input[index + 1],
+                    input[index + lineLen + 1],
+                });
+                neighbors[index] = @reduce(.Add, @as(@Vector(8, i8), @intFromBool(nearNeighbors == @as(@Vector(8, u8), @splat('@')))));
                 if (neighbors[index] < 4) {
                     count += 1;
                 }
@@ -66,64 +39,43 @@ pub fn main() !void {
     std.debug.print("part 1: {d}\n", .{count});
 
     count = 0;
-    var index: usize = 0;
+    var index: usize = lineLen;
     while (index < neighbors.len) {
         if (neighbors[index] < 4) {
             count += 1;
             neighbors[index] = 64;
-            const x = @mod(index, lineLen);
-            const y = @divFloor(index, lineLen);
             var nindex = index;
-            if (y < lines - 1) {
-                if (x < lineLen - 1) {
-                    neighbors[index + lineLen + 1] -= 1;
-                    if (neighbors[index + lineLen + 1] < 4) {
-                        nindex = index + lineLen + 1;
-                    }
-                }
-                neighbors[index + lineLen] -= 1;
-                if (neighbors[index + lineLen] < 4) {
-                    nindex = index + lineLen;
-                }
-                if (x >= 1) {
-                    neighbors[index + lineLen - 1] -= 1;
-                    if (neighbors[index + lineLen - 1] < 4) {
-                        nindex = index + lineLen - 1;
-                    }
-                }
+            neighbors[index + lineLen + 1] -= 1;
+            if (neighbors[index + lineLen + 1] < 4) {
+                nindex = index + lineLen + 1;
             }
-
-            if (x < lineLen - 1) {
-                neighbors[index + 1] -= 1;
-                if (neighbors[index + 1] < 4) {
-                    nindex = index + 1;
-                }
+            neighbors[index + lineLen] -= 1;
+            if (neighbors[index + lineLen] < 4) {
+                nindex = index + lineLen;
             }
-
-            if (x >= 1) {
-                neighbors[index - 1] -= 1;
-                if (neighbors[index - 1] < 4) {
-                    nindex = index - 1;
-                }
+            neighbors[index + lineLen - 1] -= 1;
+            if (neighbors[index + lineLen - 1] < 4) {
+                nindex = index + lineLen - 1;
             }
-
-            if (y >= 1) {
-                if (x < lineLen - 1) {
-                    neighbors[index - lineLen + 1] -= 1;
-                    if (neighbors[index - lineLen + 1] < 4) {
-                        nindex = index - lineLen + 1;
-                    }
-                }
-                neighbors[index - lineLen] -= 1;
-                if (neighbors[index - lineLen] < 4) {
-                    nindex = index - lineLen;
-                }
-                if (x >= 1) {
-                    neighbors[index - lineLen - 1] -= 1;
-                    if (neighbors[index - lineLen - 1] < 4) {
-                        nindex = index - lineLen - 1;
-                    }
-                }
+            neighbors[index + 1] -= 1;
+            if (neighbors[index + 1] < 4) {
+                nindex = index + 1;
+            }
+            neighbors[index - 1] -= 1;
+            if (neighbors[index - 1] < 4) {
+                nindex = index - 1;
+            }
+            neighbors[index - lineLen + 1] -= 1;
+            if (neighbors[index - lineLen + 1] < 4) {
+                nindex = index - lineLen + 1;
+            }
+            neighbors[index - lineLen] -= 1;
+            if (neighbors[index - lineLen] < 4) {
+                nindex = index - lineLen;
+            }
+            neighbors[index - lineLen - 1] -= 1;
+            if (neighbors[index - lineLen - 1] < 4) {
+                nindex = index - lineLen - 1;
             }
             index = @min(index, nindex) - 1;
         }
